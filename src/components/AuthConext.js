e12fb3e2c807ea6734474e49d58caf69ece50a1a
mahesh <madu140@gmail.com>
2021-08-06 10:59:50 +0530

    refactor

--- src/components/AuthConext.js
+++ src/components/AuthConext.js
@@ -10,17 +10,10 @@ function useProvideAuth(){
     
     const [currentUser, setCurrentUser] = useState(null);
 
-    useEffect(() => {
-        // sessionStorage.setItem("storeduser",null);
-        console.log("user set to null in session stotrage by AuthContext()",sessionStorage.getItem("storeduser"))
-        
-    }, [])
 
     async function login(email,password){
         await setCurrentUser("user");
-        console.log("user set in session stotrage by AuthContext() login function(state variable",currentUser)
         sessionStorage.setItem("storeduser","user");
-        console.log("authe context set user , and user name is(session storage item)",sessionStorage.getItem("storeduser"))
         return currentUser;
     }
 

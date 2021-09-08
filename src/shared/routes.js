c84c35ef960492ef2211cb46a03b694219e65ae9
roshinie <roshinie.18@cse.mrt.ac.lk>
2021-09-08 14:08:34 +0530

    Add Home Route

--- src/shared/routes.js
+++ src/shared/routes.js
@@ -2,6 +2,7 @@ export const LANDING = "/";
 export const LOGIN = "/login";
 export const SIGNUP = "/signup"
 export const PUBLIC = "/public"
+export const HOMEPAGE = "/public/home"
 export const PROTECTED = "/protected" // for testing
 
 //define the route url here and use it. Easy to change later

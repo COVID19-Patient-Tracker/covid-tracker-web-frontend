f5de04236c79f0a1975a0a4ce71e76ec52e4ad1a
roshinie <roshinie.18@cse.mrt.ac.lk>
2021-09-13 00:54:14 +0530

    Update Routes

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,5 +1,6 @@
 import React, { lazy, Suspense } from "react";
 import { Route, Switch } from "react-router-dom";
+import { useLocation } from 'react-router-dom';
 
 import Spinner from "./Spinner";
 
@@ -7,12 +8,18 @@ import * as routes from "../shared/routes";
 import { ProvideAuth } from "./AuthConext";
 import { PrivateRoute } from "./PrivateRoute";
 
+import MohAdminNav from "../components/layout/Navbar/MohAdminNav";
+import HosAdminNav from "../components/layout/Navbar/HosAdminNav";
+import HosUserNav from "../components/layout/Navbar/HosUserNav";
+import PublicNav from "../components/layout/Navbar/PublicNav";
+
 const Login = lazy(() => import("../pages/Login/Login"));
 const Signup = lazy(() => import("../pages/Signup/Signup"));
 const HomePage = lazy(() => import("../pages/Website/Home"));
 const UserManagement = lazy(() => import("../pages/Hospital/user-management"));
 const HospitalDash = lazy(() => import("../pages/Hospital/dashboard"));
 const UploadXray = lazy(() => import("../pages/Hospital/upload-xray"));
+const NotFound = lazy(() => import("../pages/InfoPages/NotFound"));
 
 function PlaceholderForProtectedRoute() {
     return (
@@ -22,20 +29,59 @@ function PlaceholderForProtectedRoute() {
 
 const Router = () => {
 
+    const location = useLocation();
+    const domain = location.pathname.split('/')
+    const path1 = domain[1];
+    const path2 = domain[2]
+
+
+    let navbar;
+    switch (path1) {
+        case "moh":
+            if (path2 === "admin") {
+                navbar = <MohAdminNav />;
+            } else if (path2 === "user") {
+                navbar = ""
+            } else {
+                navbar = "";
+            }
+            break;
+        case "hospital":
+            if (path2 === "admin") {
+                navbar = <HosAdminNav />;
+            } else if (path2 === "user") {
+                navbar = <HosUserNav />;
+            } else {
+                navbar = "";
+            }
+            break;
+        case "public":
+            navbar = <PublicNav />;
+            break;
+        default:
+            navbar = "";
+    }
+
     return (
         <Suspense fallback={<Spinner />}>
+            {navbar}
             <ProvideAuth>
                 <Switch>
-                    <Route exact path={routes.HOMEPAGE} component={HomePage} />
-                    <Route exact path={routes.USERMANAGEMENT} component={UserManagement} />
-                    <Route exact path={routes.HOSDASH} component={HospitalDash} />
-                    <Route exact path={routes.XRAY} component={UploadXray} />
+                    {/* general */}
                     <Route exact path={routes.LOGIN} component={Login} />
                     <Route exact path={routes.SIGNUP} component={Signup} />
-                    {/* <Route exact path="/" component={NotFound}/> */}
-                    {/* <Route path={routes.PUBLIC} component={NotFound}/> */}
+                    
+                    {/* hospital-admin */}
+                    <Route exact path={routes.USERMANAGEMENT} component={UserManagement} />
+                    <Route exact path={routes.HOSDASH} component={HospitalDash} />
+
+                    {/* hospital-user */}
+                    <Route exact path={routes.HOSUSERDASH} component={HospitalDash} />
+                    <Route exact path={routes.XRAY} component={UploadXray} />
+
+                    <Route exact path={routes.HOMEPAGE} component={HomePage} />
                     <PrivateRoute exact path={routes.PROTECTED} component={PlaceholderForProtectedRoute} />
-                    {/* <Route component={NotFound} /> */}
+                    <Route component={NotFound} />
                 </Switch>
             </ProvideAuth>
         </Suspense>
--- src/shared/routes.js
+++ src/shared/routes.js
@@ -1,11 +1,30 @@
+//general
 export const LANDING = "/";
 export const LOGIN = "/login";
 export const SIGNUP = "/signup"
+
+//public site
 export const PUBLIC = "/public"
 export const HOMEPAGE = "/public/home"
-export const USERMANAGEMENT = "/hospital/user-management"
-export const HOSDASH = "/hospital/dashboard"
-export const XRAY = "/hospital/upload-xray"
+
+
+//moh-admin
+
+
+
+//moh-user
+
+
+//hospital-admin
+export const HOSDASH = "/hospital/admin/dashboard"
+export const USERMANAGEMENT = "/hospital/admin/user-management"
+
+
+//hospital-user
+export const HOSUSERDASH = "/hospital/user/dashboard"
+export const XRAY = "/hospital/user/upload-xray"
+
+
 export const PROTECTED = "/protected" // for testing
 
 //define the route url here and use it. Easy to change later

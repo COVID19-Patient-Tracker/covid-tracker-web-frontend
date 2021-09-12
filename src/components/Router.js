af47fc1689c240d35953dc4a55333eebb4aee629
mahesh <madu140@gmail.com>
2021-09-12 18:34:15 +0530

    MOH user management - add MOHadmin

--- src/components/Router.js
+++ src/components/Router.js
@@ -10,7 +10,8 @@ import { PrivateRoute } from "./PrivateRoute";
 const Login = lazy(() => import("../pages/Login/Login"));
 const Signup = lazy(() => import("../pages/Signup/Signup"));
 const HomePage = lazy(() => import("../pages/Website/Home"));
-const UserManagement = lazy(() => import("../pages/Hospital/user-management"));
+const HospitalUserManagement = lazy(() => import("../pages/Hospital/user-management"));
+const MOHUserManagement = lazy(() => import("../pages/MOH/user-management"));
 
 function PlaceholderForProtectedRoute() {
     return (
@@ -25,7 +26,8 @@ const Router = () => {
             <ProvideAuth>
                 <Switch>
                     <Route exact path={routes.HOMEPAGE} component={HomePage} />
-                    <Route exact path={routes.USERMANAGEMENT} component={UserManagement} />
+                    <Route exact path={routes.HOSPITALUSERMANAGEMENT} component={HospitalUserManagement} />
+                    <Route exact path={routes.MOHUSERMANAGEMENT} component={MOHUserManagement} />
                     <Route exact path={routes.LOGIN} component={Login} />
                     <Route exact path={routes.SIGNUP} component={Signup} />
                     {/* <Route exact path="/" component={NotFound}/> */}

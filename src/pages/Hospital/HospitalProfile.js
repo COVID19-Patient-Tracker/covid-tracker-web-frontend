7d7701bb2a290c027d36ecb0a0ef42bce57eb68a
roshinie <roshinie.18@cse.mrt.ac.lk>
2021-09-26 03:57:36 +0530

    New page for hospital profile

---
+++ src/pages/Hospital/HospitalProfile.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { Typography, Container } from "@material-ui/core";
+
+
+export default function HosProfile() {
+
+
+    return (
+
+        <Container maxWidth="lg" >
+            <Typography variant="h6" gutterBottom>COVID-19 DAILY FIGURES</Typography>
+            <Typography variant="subtitle2" gutterBottom>Last Updated On: 12.09.2021 13:23:56</Typography>
+
+        </Container>
+
+    )
+}
+

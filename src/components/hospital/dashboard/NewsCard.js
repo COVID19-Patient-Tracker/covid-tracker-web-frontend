aca77e8db4f1444f7863f063a1c9b281a613bbde
roshinie <roshinie.18@cse.mrt.ac.lk>
2021-09-27 11:19:10 +0530

    Update news page

--- src/components/hospital/dashboard/NewsCard.js
+++ src/components/hospital/dashboard/NewsCard.js
@@ -1,34 +1,34 @@
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+
+import { Card, CardActionArea,CardContent,CardMedia,CardActions, Button, Typography}  from '@material-ui/core';
 
 const useStyles = makeStyles({
     root: {
-        maxWidth: 345,
+        maxWidth: 400,
+        borderWidth: 4,
+        borderRadius: 10,
     },
     media: {
         height: 200,
     },
+    btn: {
+        backgroundColor: "#5985bd"
+    }
 });
 
 export default function MediaCard() {
     const classes = useStyles();
 
     return (
-        <Card className={classes.root}>
+        <Card className={classes.root} variant="outlined">
             <CardActionArea>
                 <CardMedia
                     className={classes.media}
-                    image="/assets/newsimage.png"
+                    image="/assets/cardnews.jpg"
                     title="Contemplative Reptile"
                 />
                 <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2">
+                    <Typography gutterBottom variant="h5" component="h2" style={{fontWeight: 700}}>
                         Pay special attention to those who suffer from chronic non-communicable diseases…
                     </Typography>
                     <Typography variant="body2" color="textSecondary" component="p">
@@ -37,10 +37,10 @@ export default function MediaCard() {
                 </CardContent>
             </CardActionArea>
             <CardActions>
-                <Button  variant="contained" size="small" >
+                <Button  variant="contained" size="small" className={classes.btn}>
                     Share
                 </Button>
-                <Button variant="contained" size="small">
+                <Button variant="contained" size="small" className={classes.btn}>
                     Read More
                 </Button>
             </CardActions>
